fe6c55c812a34e0cfa4369a2ad74b2b717f7eeb5
crisroes <cristian.roescobar@gmail.com>
2022-09-20 20:08:52 -0500

    style: change of appearance for a better presentation

--- src/views/preguntas/PreguntaTipoView.vue
+++ src/views/preguntas/PreguntaTipoView.vue
@@ -1,62 +1,134 @@
 <script setup lang="ts">
-import CursoImage from '@/assets/images/Geometria.jpg';
+import { useInterObserver } from '@/composables';
+import PrincipianteIconVue from '@/components/draws/icons/PrincipianteIcon.vue';
+import IntermedioIcon from '../../components/draws/icons/IntermedioIcon.vue';
+import AvanzadoIcon from '../../components/draws/icons/AvanzadoIcon.vue';
+const observer = useInterObserver(
+  (entry) => {
+    entry.target.classList.toggle('show', entry.isIntersecting);
+    if (entry.isIntersecting) {
+      observer.unobserve(entry.target);
+    }
+  },
+  {
+    queryAllSelector: 'section.section',
+    init: {
+      root: document.querySelector('main.container'),
+      rootMargin: '0px',
+    },
+  }
+);
 </script>
 <template>
-  <div class="container">
-    <h1 class="py-5 text-lg font-medium uppercase md:text-3xl">¿Cómo generar la pregunta?</h1>
-    <div class="listCard">
-      <div class="card">
-        <router-link :to="{ name: 'Pregunta', params: { tipo: 'general' } }">
-          <img class="image" :src="CursoImage" alt="imagen pregunta general" />
-          <strong class="text-lg font-bold text-contrast-01">Nivel Experto</strong>
-          <section class="m-2 px-2 py-2">
-            <strong>Descripción</strong>
-            <p class="text-base">
-              Genera una pregunta de manera general de los cursos y temas existentes en la base de
-              datos.
-            </p>
-          </section>
-        </router-link>
+  <section class="section font-inter h-full w-full p-4 pt-0 text-gray-400 antialiased">
+    <div class="mx-auto px-4">
+      <div id="title" class="mb-5 text-center">
+        <h1 class="text-4xl font-bold text-secondary dark:text-white">Elige un Nivel</h1>
+        <p class="text-light text-xl text-gray-500">
+          Es importante eleguir un nivel adecuado a su capacida para un mejor aprendizaje
+        </p>
       </div>
-      <div class="card">
-        <router-link :to="{ name: 'Cursos', params: { tipo: 'curso' } }">
-          <img class="image" :src="CursoImage" alt="imagen pregunta por curso" />
-          <strong class="text-lg font-bold text-contrast-01">Nivel Intermedio</strong>
-          <section class="m-2 px-2 py-2">
-            <strong>Descripción</strong>
-            <p class="text-base">
-              Genera una pregunta de un curso de su elección exitentes en la base de datos.
-            </p>
-          </section>
-        </router-link>
-      </div>
-      <div class="card">
-        <router-link :to="{ name: 'Cursos', params: { tipo: 'tema' } }">
-          <img class="image" :src="CursoImage" alt="imagen pregunta por tema" />
-          <strong class="text-lg font-bold text-contrast-01">Nivel Básico</strong>
-          <section class="m-2 px-2 py-2">
-            <strong>Descripción</strong>
-            <p class="text-base">
-              Genera una pregunta de un tema de los cursos existentes en la base de datos.
-            </p>
-          </section>
-        </router-link>
+      <div class="grid grid-cols-1 justify-evenly gap-10 pt-10 md:grid-cols-2 lg:grid-cols-3">
+        <div
+          class="w-full transform overflow-hidden rounded-lg text-center transition duration-200 ease-in hover:scale-105 hover:shadow-2xl"
+        >
+          <div class="w-full border-b border-secondary py-5">
+            <h2 class="text-3xl font-bold text-secondary dark:text-white">Principiante</h2>
+            <h3 class="mt-2 text-xl font-normal text-indigo-500">Preguntas por Temas</h3>
+          </div>
+          <div id="content" class="">
+            <div id="icon" class="my-5">
+              <PrincipianteIconVue />
+              <p class="pt-2 text-sm text-gray-500">
+                Perfecto para estudiantes que desean preguntas sobre un tema o que esten comenzando
+              </p>
+            </div>
+            <div id="contain" class="mb-10 text-lg font-light leading-8">
+              <div id="choose" class="mt-10 w-full px-6">
+                <router-link
+                  :to="{ name: 'Cursos', params: { tipo: 'tema' } }"
+                  class="block w-full rounded-xl bg-secondary py-4 text-xl font-medium transition duration-200 ease-in-out hover:bg-secondary-normal hover:text-white hover:shadow-lg"
+                >
+                  Elegir
+                </router-link>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          class="w-full transform overflow-hidden rounded-lg text-center transition duration-200 ease-in hover:scale-105 hover:shadow-2xl"
+        >
+          <div class="w-full border-b border-secondary py-5">
+            <h2 class="text-3xl font-bold text-secondary dark:text-white">Intermedio</h2>
+            <h3 class="mt-2 text-xl font-normal text-indigo-500">Preguntas por Curso</h3>
+          </div>
+          <div id="content" class="">
+            <div id="icon" class="my-5">
+              <IntermedioIcon />
+              <p class="pt-2 text-sm text-gray-500">
+                Perfecto para estudiantes que desean preguntas sobre un curso y que tengan mayor
+                conocimiento
+              </p>
+            </div>
+            <div id="contain" class="mb-10 text-lg font-light leading-8">
+              <div id="choose" class="mt-10 w-full px-6">
+                <router-link
+                  :to="{ name: 'Cursos', params: { tipo: 'curso' } }"
+                  class="block w-full rounded-xl bg-secondary py-4 text-xl font-medium transition duration-200 ease-in-out hover:bg-secondary-normal hover:text-white hover:shadow-lg"
+                >
+                  Elegir
+                </router-link>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          class="w-full transform overflow-hidden rounded-lg text-center transition duration-200 ease-in hover:scale-105 hover:shadow-2xl"
+        >
+          <div class="w-full border-b border-secondary py-5">
+            <h2 class="text-3xl font-bold text-secondary dark:text-white">Avanzado</h2>
+            <h3 class="mt-2 text-xl font-normal text-indigo-500">Preguntas en General</h3>
+          </div>
+          <div id="content" class="">
+            <div id="icon" class="my-5">
+              <AvanzadoIcon />
+              <p class="pt-2 text-sm text-gray-500">
+                Perfecto para estudiantes expertos y quieran competir por ser el rey de las
+                preguntas
+              </p>
+            </div>
+            <div id="contain" class="mb-10 text-lg font-light leading-8">
+              <div id="choose" class="mt-10 w-full px-6">
+                <router-link
+                  :to="{ name: 'Pregunta', params: { tipo: 'general' } }"
+                  class="block w-full rounded-xl bg-secondary py-4 text-xl font-medium transition duration-200 ease-in-out hover:bg-secondary-normal hover:text-white hover:shadow-lg"
+                >
+                  Elegir
+                </router-link>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
+  </section>
 </template>
 <style>
-.image {
-  @apply w-full;
-}
-.card {
-  @apply w-full transform overflow-hidden
-  rounded bg-blue-400 text-center shadow-lg shadow-blue-500/50 transition duration-500 ease-in-out hover:-translate-y-1
-  hover:bg-blue-500;
+.section {
+  @apply flex min-h-screen flex-col justify-start gap-2 py-3 md:gap-10;
+  opacity: 0;
+  transform: translateX(-100px);
 }
 
-.listCard {
-  @apply grid justify-between gap-8 overflow-auto py-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4
-  xl:grid-cols-3;
+.section.show {
+  opacity: 1;
+  transform: translateX(0);
+}
+
+.section {
+  transition-delay: 0.25s;
+  transition-duration: 1.5s;
+  transition-property: transform, opacity;
+  transition-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1);
 }
 </style>
