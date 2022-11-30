4f9a88cd72cba35f0a1f781036b34e64913587b2
crisroes <cristian.roescobar@gmail.com>
2022-11-30 12:18:41 -0500

    feat: add pwa to improve applicaction performance

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ import NBApp from '@/NBApp.vue';
 import '@/styles/main.css';
 import { setTokenAuth } from './plugins';
 
+//registerSW
+import { registerSW } from 'virtual:pwa-register';
 const app = createApp(NBApp);
 const piniaStore = createPinia();
 
@@ -15,3 +17,19 @@ app.use(piniaStore);
 app.use(router);
 piniaStore.use(setTokenAuth);
 app.mount('#noteblue-app', true);
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    // show a prompt to user
+    if (window.confirm('Tenemos una actualización ¿desea actualizar?')) {
+      updateSW();
+    }
+  },
+  onOfflineReady() {
+    alert('Listo para trabajar sin conexión');
+    // show a ready to work offline to user
+  },
+});
+// when user clicked the "refresh" button
+updateSW();
+// the page will reload and the up-to-date content will be served.
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,35 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
-
+import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg,jpeg,}'],
+        skipWaiting: true,
+      },
+      includeAssets: ['logo.png'],
+      manifest: {
+        name: 'NoteBlue',
+        short_name: 'NoteBlue',
+        description: 'NoteBlue App',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     port: 7880,
   },
