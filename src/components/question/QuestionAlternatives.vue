fc075dd4626c1adcb811e787ba02d43a6fe0743a
Cristian Romero Escobar <cristian.roescobar@gmail.com>
2023-09-29 11:54:27 -0500

    style: Añade un nuevo estilo CSS a los botones para darles un aspecto de foco iluminado al pasar el mouse por encima y seleccionar

--- src/components/question/QuestionAlternatives.vue
+++ src/components/question/QuestionAlternatives.vue
@@ -20,7 +20,7 @@ defineEmits(['select-item', 'click-label']);
       <div class="w-full items-center px-2 py-1 md:w-1/2">
         <input
           :id="alternativa?.id"
-          class="btn-select peer hidden"
+          class="peer hidden"
           type="radio"
           name="alternativaId"
           :value="alternativa?.id"
@@ -29,7 +29,9 @@ defineEmits(['select-item', 'click-label']);
           @click.once="$emit('select-item', alternativa.id)"
         />
         <label
-          class="label show flex justify-center gap-1 rounded-xl bg-secondary bg-opacity-90 p-2 text-sky-300 shadow-xl hover:bg-opacity-75 peer-checked:bg-secondary peer-checked:text-white"
+          id="btn"
+          name="alternativaId"
+          class="label peer-checked flex justify-center rounded-xl bg-secondary bg-opacity-90 p-2 text-sky-500 shadow-xl hover:bg-opacity-75"
           :for="alternativa?.id"
         >
           <strong>
@@ -42,9 +44,34 @@ defineEmits(['select-item', 'click-label']);
 </template>
 <style scoped>
 .label {
-  @apply m-1 rounded-md px-3 outline outline-1 outline-offset-2;
+  @apply m-1 rounded-md border-cyan-500 px-3 outline outline-1 outline-offset-2;
+  padding: 10px 20px;
+  text-transform: uppercase;
+  border-radius: 8px;
+  font-size: 17px;
+  font-weight: 500;
+  text-shadow: none;
+  background: transparent;
+  box-shadow: transparent;
+  border: 1px solid #0abcf880;
+  transition: 0.5s ease;
+  user-select: none;
 }
-.btn-select {
-  filter: drop-shadow(0 0 1em #646cffaa);
+
+.peer:checked ~ .peer-checked {
+  color: #ffffff;
+  background: #008cff;
+  border: 1px solid #008cff;
+  text-shadow: 0 0 5px #ffffff, 0 0 10px #ffffff, 0 0 20px #ffffff;
+  box-shadow: 0 0 5px #008cff, 0 0 20px #008cff, 0 0 50px #008cff, 0 0 100px #008cff;
+}
+
+#btn:hover,
+:focus {
+  color: #ffffff;
+  background: #008cff;
+  border: 1px solid #008cff;
+  text-shadow: 0 0 5px #ffffff, 0 0 10px #ffffff, 0 0 20px #ffffff;
+  box-shadow: 0 0 5px #008cff, 0 0 20px #008cff, 0 0 50px #008cff, 0 0 100px #008cff;
 }
 </style>
